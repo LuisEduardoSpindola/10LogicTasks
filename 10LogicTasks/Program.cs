781576c15dc2fc64a3ab11923e03bddb97e2d1b8
Luis Eduardo Spindola <eduspindola@live.com>
2024-02-20 23:19:22 -0300

    Adicionar arquivos de projeto.

---
+++ 10LogicTasks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program 
+{
+    public static void Main()
+    {
+        //Coletando o maior número
+        int result = Task1();
+        Console.WriteLine("Max Number: " + result);
+    }
+
+    public static int Task1()
+    {
+        List<int> nums = new List<int> { 1, 2, 3 };
+        int maxNum = nums[0];
+
+        for (int i = 0; i < nums.Count; i++)
+        {
+            if (nums[i] > maxNum)
+            {
+                maxNum = nums[i];
+            }
+        }
+        return maxNum;
+    }
+}
